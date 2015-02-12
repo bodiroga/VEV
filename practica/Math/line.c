e468a9a5e3fa2b59739e4508f56d144ca6f8075f
Aitor <aiturrioz005@ikasle.ehu.es>
2015-02-12 19:56:08 +0100

    Archivo line.c (con sus funciones) completo.

--- practica/Math/line.c
+++ practica/Math/line.c
@@ -10,6 +10,13 @@ line *CreateLine(float ox, float oy, float oz, float dx, float dy, float dz) {
 
 	thisLine = malloc( sizeof(*thisLine) * 1);
 
+	thisLine->ox = ox;
+	thisLine->oy = oy;
+	thisLine->oz = oz;
+	thisLine->dx = dx;
+	thisLine->dy = dy;
+	thisLine->dz = dz;
+
 	return thisLine;
 }
 
@@ -20,6 +27,19 @@ line *CreateLine(float ox, float oy, float oz, float dx, float dy, float dz) {
 
 line *CreateLinePP(float Ax, float Ay, float Az, float Bx, float By, float Bz) {
 
+	// p(u) = o + ud
+	// o serán las coordenadas de A, el origen del vector
+	// d se calculará usando UnitVectorPP de tools.c
+
+	float Dx, Dy, Dz;
+	float distance;
+
+	distance = UnitVectorPP(&Dx, &Dy, &Dz, Ax, Ay, Az, Bx, By, Bz);
+
+	if (distance > DISTANCE_EPSILON)
+		return CreateLine(Ax, Ay, Az, Dx, Dy, Dz);
+	else
+		return CreateLine(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f);
 }
 
 void DestroyLine(line **theLine) {
@@ -32,6 +52,11 @@ void DestroyLine(line **theLine) {
 // @@ TODO: Give the point corresponding to parameter t. Leave output in Px,Py,Pz
 
 void PointInLine(line *theLine, float t, float *Px, float *Py, float *Pz) {
+
+	*Px = theLine->ox + t*(theLine->dx);
+	*Py = theLine->oy + t*(theLine->dy);
+	*Pz = theLine->oz + t*(theLine->dz);
+
 }
 
 // @@ TODO: Calculate the parameter 'u0' of the line point nearest to P
@@ -39,6 +64,19 @@ void PointInLine(line *theLine, float t, float *Px, float *Py, float *Pz) {
 // u0 = D*(P-O) / D*D , where * == dot product
 
 float ParamDistance(line * theLine, float Px, float Py, float Pz) {
+
+	float resta_x, resta_y, resta_z;
+	float producto_numerador, producto_denominador;
+
+	resta_x = Px - theLine->ox;
+	resta_y = Py - theLine->oy;
+	resta_z = Pz - theLine->oz;
+
+	producto_numerador = DOT_PRODUCT(theLine->dx, theLine->dy, theLine->dz, resta_x, resta_y, resta_z);
+
+	producto_denominador = DOT_PRODUCT(theLine->dx, theLine->dy, theLine->dz, theLine->dx, theLine->dy, theLine->dz);
+
+	return producto_numerador/producto_denominador;
 
 }
 
@@ -48,6 +86,17 @@ float ParamDistance(line * theLine, float Px, float Py, float Pz) {
 // Where u = ParamDistance(theLine, float Px, float Py, float Pz)
 
 float DistanceP(line * theLine, float Px, float Py, float Pz) {
+
+	float u, distance_x, distance_y, distance_z;
+
+	u = ParamDistance(theLine, Px, Py, Pz);
+
+	distance_x = Px - (theLine->ox + u*theLine->dx);
+	distance_y = Py - (theLine->oy + u*theLine->dy);
+	distance_z = Pz - (theLine->oz + u*theLine->dz);
+
+	return AGA_MOD(distance_x, distance_y, distance_z);
+
 
 }
 
